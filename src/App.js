e8f1ffec5024a6878a017f4443c6486c3bb5cad3
Muhammad-Ahmad13 <ahmadilyasgee@gmail.com>
2022-05-20 18:55:38 +0500

    validation added and yup validation added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Forgot from './components/Navigationpages/Forgot';
 import ModalSignLog from './components/Navigationpages/ModalSignLog';
 import SignUP from './components/Navigationpages/SignUP';
 import Starter from './components/Starter';
+import UserInfo from './components/UserInfo/UserInfo';
 function App() {
   return (
     <div className="App">
@@ -13,6 +14,7 @@ function App() {
         <Route exact path ='/' element ={<HomePage />}></Route>
         <Route exact path ='/signup' element ={<SignUP />}></Route>
         <Route exact path ='/login' element ={<ModalSignLog />}></Route>
+        <Route exact path='/userinfo' element ={<UserInfo/>}></Route>
         {/* <Route exact path ='/forgot' element ={<Forgot />}></Route> */}
         <Route exact path ='/changepass' element ={<ChangePassword />}></Route>
         <Route  path='/starter' element={<Starter />}></Route>
--- src/components/Navigationpages/Forgot.js
+++ src/components/Navigationpages/Forgot.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import "./SignUP.css";
 const Forgot = () =>{
+    // const firstRef = useRef(null);
+    const [forgetMail, setForgetMail] = useState('');
+    const handleForgetSubmit = (f) =>{
+        f.preventDefault();
+        console.log(forgetMail);
+        setForgetMail('');
+        // firstRef.current.value = ''
+        // firstRef.value= setForgetMail(forgetMail);
+    }
     return(
         <div className="sign-main">
             <div className="sign-sec shadow-none">
                 {/* <h4>Forget Password</h4> */}
-                <form>
+                <form onSubmit={handleForgetSubmit}>
                     <div className="sign-input">
                         <label>Email</label>
-                        <input placeholder="Enter your Email" required/>
+                        <input 
+                        //   ref={firstRef}
+                          type="email"
+                          placeholder="Enter your Email"
+                          name="getMail"
+                          autoComplete="off"
+                          value={forgetMail}
+                          onChange ={(f) => setForgetMail(f.target.value)} 
+                          required
+                        />
                     </div>
-                    
                     <div className="sign-anc text-center">
-                        <button>Submit</button>
+                        <button type="submit">Reset Password</button>
                     </div>
                     {/* <Link to="">back to login</Link> */}
                 </form>
